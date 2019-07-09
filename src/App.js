cfd000083d220c71f895e4dfc008ffbb6a293e15
Petrosz <andipeti2000@gmail.com>
2019-07-10 01:22:55 +0200

    Started rolling out the new design

--- src/App.js
+++ src/App.js
@@ -32,6 +32,15 @@ class App extends Component {
 
         return (
             <div className="container">
+                {/* <div id="navbar" className="navbar">
+                    <ul className="nav-items">
+                        <li className="nav-item">Categories</li>
+                        <li className="nav-item">Addon</li>
+                        <li className="nav-item">Undercut Checker</li>
+                        <li className="nav-item">Seller Search</li>
+                        <li className="nav-item">Custom Tables</li>
+                    </ul>
+                </div> */}
                 <div id="searchBox" className="searchBox">
                     <h1 className="title">Goblineer</h1>
                     <Search handleChange={this.handleChange} handleSearch={this.handleSearch} />
@@ -59,6 +68,7 @@ class App extends Component {
             document.getElementById('searchBox').style.top = '50%';
             document.getElementById('searchBox').style.margin = '-167px 0 0 0';
         } else if(value.length >= 4) {
+            // document.getElementsByClassName('title')[0].style.margin = '-131px 0 0 0';
             document.getElementsByClassName('title')[0].style.margin = '-115px 0 0 0';
             document.getElementById('searchField').style.width = '100%';
             document.getElementById('searchField').style.borderRadius = '0';
